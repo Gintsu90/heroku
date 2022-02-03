5d6a4d6abcf5faf0fbd70a15322f2ae72b8ac386
Toni Pekkala <tonipekkala90@gmail.com>
2022-02-03 15:45:24 +0200

    production build created

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 morgan.token("person", (req, res) => JSON.stringify(req.body));
 
 app.use(express.json())
+app.use(express.static("build"))
 app.use(cors())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :person"))
 
